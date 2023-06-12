2fe8f9b26321158b197ec1662d28e699aee9635d
Jayson Thompson <jaysonthompson922@gmail.com>
2023-06-11 19:21:48 -0700

    wires up email functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,11 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
+//ZitY3VlpxS48D-neh
+//template_8fsnh1p
+//service_9jrfpfv
+
+
 const Contact = () => {
   const formRef = useRef()
   const [form, setForm] = useState({
@@ -16,9 +21,45 @@ const Contact = () => {
   })
   const [ loading, setLoading ] = useState(false)
   
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    
+    setForm({ ...form, [name]: value })
+  } 
   
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs.send(
+      'service_9jrfpfv',
+      'template_8fsnh1p',
+      {
+        from_name: form.name,
+        to_name: "Jayson",
+        from_email: form.email,
+        to_email: "jaysonthompson922@gmail.com",
+        message: form.message,
+      },
+      'ZitY3VlpxS48D-neh'
+    )
+    .then(() => {
+      setLoading(false)
+      alert('Thank you. I will get back to you as soon as possible.')
+      
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      })
+    }), (error) => {
+      setLoading(false)
+      
+      console.log(error)
+      
+      alert('Something went wrong.')
+    } 
+  }
   
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
